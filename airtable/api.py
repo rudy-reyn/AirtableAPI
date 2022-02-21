e9c5113cc3ab17ccdaa2ed84481eba4a221e40f6
Rudy Reynolds <rure6748@colorado.edu>
2022-02-21 16:28:37 -0700

    Refactored request functionality

--- airtable/api.py
+++ airtable/api.py
@@ -6,28 +6,30 @@ import requests
 from requests import Session, Response
 
 class AirtableBaseAPI:
-    def __init__(self, baseID: str, api_key: str, tables: dict):
+    def __init__(self, baseID: str, api_key: str, schema: dict | tuple=(),  enforce_schema=False):
         self.host = "https://api.airtable.com/v0"
         self.baseID = baseID
-        self.tables = tables
+
+        self.schema = schema
+        self.enforce_schema = enforce_schema
+
         self.api = f"{self.host}/{baseID}"
         self.auth = {"Authorization": f"Bearer {api_key}"}
         self.session = Session()
 
-    def _request(self, method, table, *args, headers=None, raise_for_status=False, **kwargs) -> Response:
-        if not table in self.tables:
+    def _request(self, method, table, *args, raise_for_status=False, **kwargs) -> Response:
+        if self.enforce_schema and not table in self.schema:
             raise ValueError(f"Table {table} does not exist")
-        headers = headers or {}
-        response = method(f"{self.api}/{table}", *args, headers=self.auth | headers, **kwargs)
+        headers = self.auth | kwargs.pop("headers", {})
+        response = method(f"{self.api}/{table}", *args, headers=headers, **kwargs)
         if raise_for_status:
             response.raise_for_status()
         return response
 
-    def _update_request(self, method, table, data, *args,
-            headers: dict | None=None, maximum=10, **kwargs) -> Response:
+    def _update_request(self, method, table, data, *args, maximum=10, **kwargs) -> Response:
         if not 0 < len(data) <= maximum:
-            raise TypeError("Only between one and ten values can be included.")
-        headers = {"Content-Type": "application/json"} | (headers or {})
+            raise ValueError("Only between one and ten values can be included.")
+        headers = {"Content-Type": "application/json"} | kwargs.pop("headers", {})
         return self._request(method, table, *args, headers=headers, data=json.dumps(data), **kwargs)
 
     def __enter__(self):
@@ -42,7 +44,7 @@ class Airtable(AirtableBaseAPI):
 
     def post(self, table, data, *args, maximum=10, **kwargs) -> Response:
         if not 0 < len(data) <= maximum:
-            raise TypeError("Only between one and ten values can be included.")
+            raise ValueError("Only between one and ten values can be included.")
         records = {"records": [{"fields": i} for i in data]}
         return self._update_request(self.session.post, table, records, *args, **kwargs)
 
@@ -58,7 +60,8 @@ class Airtable(AirtableBaseAPI):
         return self._request(self.session.delete, table, *args, params=params, **kwargs)
 
     def dump_tables(self, tables=(), *args, **kwargs) -> Generator:
-        for table in tables or self.tables:
+        for table in tables or self.schema:
             yield table, self.get(table, *args, **kwargs)
 
+    # Aliases for the corresponding request methods
     select, insert, update, update_and_clear = get, post, patch, put
